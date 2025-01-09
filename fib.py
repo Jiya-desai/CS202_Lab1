bf8ed561a08ad6034e0205c6f2d5f5c6284c6c75
Jiya-desai <jiya.desai@iitgn.ac.in>
2025-01-09 09:45:57 +0530

    adding fib.py file

---
+++ fib.py
@@ -0,0 +1,24 @@
+# printing the nth fibonacci number:
+
+def fibonacci(n):
+    # for invalid input:
+    if n <= 0:
+        print("Please enter a positive integer greater than 0.")
+        return
+    
+    # base case initialisation:
+    a, b = 0, 1
+    
+    # next fib number:
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+
+    # base case
+    if n == 1:
+        print(f"The {n}th Fibonacci number is: {a}")
+    else:
+        print(f"The {n}th Fibonacci number is: {b}")
+
+
+n = int(input("Enter 'n' to print the nth Fibonacci number: "))
+fibonacci(n)
